1ca6e2a3db010e6468297cb35069ad3b0f38a215
kolla <kolla>
2007-11-05 12:28:51 +0000

    Fjernet gamle gentoo-greier Lagt til portage (gentoo) med de ulike versjonene Lagt til pkgsrc som brukes blant annet av NetBSD
    git-svn-id: https://svn.testnett.uninett.no/radsecproxy/trunk@193 e88ac4ed-0b26-0410-9574-a7f39faa03bf

---
+++ packaging/pkgsrc/net/radsecproxy/files/radsecproxy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#
+# PROVIDE: radsecproxy
+# REQUIRE: network
+#
+
+if [ -f /etc/rc.subr ]
+then
+	. /etc/rc.subr
+fi
+
+name="radsecproxy"
+rcvar=${name}
+command="/usr/pkg/sbin/${name}"
+command_args="-c /usr/pkg/etc/${name}/${name}.conf"
+
+if [ -f /etc/rc.subr ]
+then
+	load_rc_config $name
+	run_rc_command "$1"
+else
+	echo -n ' ${name}'
+	exec ${command} ${command_args}
+fi
