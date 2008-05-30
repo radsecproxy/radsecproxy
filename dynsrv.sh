9127b7c859dea5d35acc266c6564bd6d4ca111a0
venaas <venaas>
2008-05-30 14:28:45 +0000

    made example dynamic server srv record script also use port number
    git-svn-id: https://svn.testnett.uninett.no/radsecproxy/trunk@268 e88ac4ed-0b26-0410-9574-a7f39faa03bf

--- dynsrv.sh
+++ dynsrv.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
-host=`host -t srv _radsec._tcp.$1|cut -d\  -f8`
+srv=`host -t srv _radsec._tcp.$1`
+host=`echo $srv|cut -d\  -f8`
+port=`echo $srv|cut -d\  -f7`
 echo "server $1-$host {"
 echo "    host $host"
+echo "    port $port"
 echo "}"
