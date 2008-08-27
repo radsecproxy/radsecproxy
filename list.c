0e6647a3d9bae452a5db41d9786a0c2cc1c43338
venaas <venaas>
2008-08-27 12:17:34 +0000

    minor fix in list.c
    git-svn-id: https://svn.testnett.uninett.no/radsecproxy/branches/release-1.1@360 e88ac4ed-0b26-0410-9574-a7f39faa03bf

--- list.c
+++ list.c
@@ -13,6 +13,9 @@ struct list *list_create() {
 /* frees all memory associated with the list */
 void list_destroy(struct list *list) {
     struct list_node *node, *next;
+
+    if (!list)
+	return;
     
     for (node = list->first; node; node = next) {
 	free(node->data);
@@ -47,7 +50,7 @@ void *list_shift(struct list *list) {
     struct list_node *node;
     void *data;
     
-    if (!list->first)
+    if (!list || !list->first)
 	return NULL;
     
     node = list->first;
