37f71f78b5633740ffdb09ca39035bec197db0a4
venaas <venaas>
2008-09-08 06:35:48 +0000

    make all regexps extended
    git-svn-id: https://svn.testnett.uninett.no/radsecproxy/branches/release-1.1@362 e88ac4ed-0b26-0410-9574-a7f39faa03bf

--- radsecproxy.c
+++ radsecproxy.c
@@ -2701,7 +2701,7 @@ void addrealm(char *value, char **servers, char **accservers, char *message, uin
     realm->message = message;
     realm->accresp = accresp;
 
-    if (regcomp(&realm->regex, regex ? regex : value + 1, REG_ICASE | REG_NOSUB))
+    if (regcomp(&realm->regex, regex ? regex : value + 1, REG_EXTENDED | REG_ICASE | REG_NOSUB))
 	debugx(1, DBG_ERR, "addrealm: failed to compile regular expression %s", regex ? regex : value + 1);
     if (regex)
 	free(regex);
@@ -2739,7 +2739,7 @@ int addmatchcertattr(struct clsrvconf *conf, char *matchcertattr) {
 	debug(DBG_ERR, "malloc failed");
 	return 0;
     }
-    if (regcomp(*r, v, REG_ICASE | REG_NOSUB)) {
+    if (regcomp(*r, v, REG_EXTENDED | REG_ICASE | REG_NOSUB)) {
 	free(*r);
 	*r = NULL;
 	debug(DBG_ERR, "failed to compile regular expression %s", v);
