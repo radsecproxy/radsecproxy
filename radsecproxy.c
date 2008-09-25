bf093fa8a4f8891f679087df5a747d45408c5010
venaas <venaas>
2008-09-25 05:53:41 +0000

    work around to check tls blocks at startup
    git-svn-id: https://svn.testnett.uninett.no/radsecproxy/trunk@406 e88ac4ed-0b26-0410-9574-a7f39faa03bf

--- radsecproxy.c
+++ radsecproxy.c
@@ -3367,7 +3367,8 @@ int conftls_cb(struct gconffile **cf, void *arg, char *block, char *opt, char *v
 	debug(DBG_ERR, "conftls_cb: malloc failed");
 	goto errexit;
     }
-	    
+    if (!tlsgetctx(RAD_TLS, conf))
+	debug(DBG_ERR, "conftls_cb: error creating ctx for TLS block %s", val);
     debug(DBG_DBG, "conftls_cb: added TLS block %s", val);
     return 1;
 
