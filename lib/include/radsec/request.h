b98c73463183af1806ab61cdb6e98259ba250e31
Linus Nordberg <linus@nordu.net>
2011-02-02 10:55:27 +0100

    Reorder function prototypes.

--- lib/include/radsec/request.h
+++ lib/include/radsec/request.h
@@ -7,8 +7,8 @@ extern "C" {
 #endif
 
 int rs_request_create(struct rs_connection *conn, struct rs_request **req_out);
+int rs_request_send(struct rs_request *request, struct rs_packet *req_msg, struct rs_packet **resp_msg);
 void rs_request_destroy(struct rs_request *request);
-int rs_request_send(struct rs_request *request, struct rs_packet *req, struct rs_packet **resp);
 
 #if defined (__cplusplus)
 }
