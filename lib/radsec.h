725bec6f03436f9aedb3ffc7196049ab0f32f218
Linus Nordberg <linus@nordu.net>
2011-03-07 09:38:01 +0100

    Remove lib/radsec.h.
    1. Two radsec.h is icky.
    2. Its contents fit nicely in send.c.

--- lib/radsec.h
+++
@@ -1,9 +0,0 @@
-/* Copyright 2011 NORDUnet A/S. All rights reserved.
-   See the file COPYING for licensing information.  */
-
-/* RFC 5080 2.2.1.  Retransmission Behavior */
-#define IRT 2
-#define MRC 5
-#define MRT 16
-#define MRD 30
-
