7bd18eed0316ca7cae8c416b1853842c7c375355
Linus Nordberg <linus@nordu.net>
2011-03-09 12:48:00 +0100

    Add missing header files.

---
+++ lib/conn.h
@@ -0,0 +1,6 @@
+/* Copyright 2011 NORDUnet A/S. All rights reserved.
+   See the file COPYING for licensing information.  */
+
+int conn_user_dispatch_p (const struct rs_connection *conn);
+int conn_close (struct rs_connection **connp);
+int conn_activate_timeout (struct rs_connection *conn);
