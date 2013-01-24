9cf549b20eb0ae56afac8c3ddff8725c8c1380db
Linus Nordberg <linus@nordberg.se>
2013-01-24 17:32:18 +0100

    Do the test for PSK properly.
    Fixes 823ea9ba.

--- lib/event.c
+++ lib/event.c
@@ -246,9 +246,7 @@ event_on_connect (struct rs_connection *conn, struct rs_packet *pkt)
   assert (!conn->is_connecting);
 
 #if defined (RS_ENABLE_TLS)
-  if ((conn->realm->type == RS_CONN_TYPE_TLS
-       || conn->realm->type == RS_CONN_TYPE_DTLS)
-      && conn->realm->transport_cred->type != RS_CRED_TLS_PSK)
+  if (conn_type_tls(conn) && !conn_cred_psk(conn))
     if (tls_verify_cert (conn) != RSE_OK)
       {
         rs_debug (("%s: server cert verification failed\n", __func__));
