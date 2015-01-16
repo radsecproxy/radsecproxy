8bbdecd3a0f12411fb004b4ae47dc3ce383661e4
Linus Nordberg <linus@nordberg.se>
2015-01-16 12:23:37 +0100

    When CHAP-Password, copy Request Authenticator to CHAP-Challenge.
    Conflicts:
    	radmsg.h

--- radmsg.h
+++ radmsg.h
@@ -17,10 +17,12 @@
 
 #define RAD_Attr_User_Name 1
 #define RAD_Attr_User_Password 2
+#define RAD_Attr_CHAP_Password 3
 #define RAD_Attr_Reply_Message 18
 #define RAD_Attr_Vendor_Specific 26
 #define RAD_Attr_Calling_Station_Id 31
 #define RAD_Proxy_State 33
+#define RAD_Attr_CHAP_Challenge 60
 #define RAD_Attr_Tunnel_Password 69
 #define RAD_Attr_Message_Authenticator 80
 
