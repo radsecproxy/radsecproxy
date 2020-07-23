219fd23cb860837051f5273506c02298de8b09cd
Fabian Mauchle <fabian.mauchle@switch.ch>
2020-07-23 18:33:25 +0200

    Fix modifyVendorAttribute not applied (#62)

--- tests/t_rewrite_config.c
+++ tests/t_rewrite_config.c
@@ -15,40 +15,73 @@ main (int argc, char *argv[])
     struct rewrite *result;
     char *rewritename = "rewrite";
     char **addattrs;
-    int numtests = 1, i;
+    int numtests = 2, i;
     struct tlv *tlv, *expected;
     uint8_t expectedvalue[] = {'1',0,0,'1','A','%','4','1'};
 
     printf("1..%d\n", numtests);
     numtests = 1;
 
-    addattrs = malloc(2);
-    addattrs[0] = stringcopy("1:'1%00%001%41%2541", 0);
-    addattrs[1] = NULL;
+    {
+        addattrs = malloc(2);
+        addattrs[0] = stringcopy("1:'1%00%001%41%2541", 0);
+        addattrs[1] = NULL;
 
-    expected = maketlv(1,8,expectedvalue);
+        expected = maketlv(1,8,expectedvalue);
 
-    addrewrite(rewritename, 0, NULL, NULL, addattrs,
-                    NULL, NULL, NULL, NULL, NULL);
+        addrewrite(rewritename, 0, NULL, NULL, addattrs,
+                        NULL, NULL, NULL, NULL, NULL);
 
-    result = getrewrite(rewritename, NULL);
+        result = getrewrite(rewritename, NULL);
 
-    if (result->addattrs->first) {
-        tlv = (struct tlv *)result->addattrs->first->data;
-        if (!eqtlv(tlv, expected)) {
-            printf ("tlv value was: 0x");
-            for (i = 0; i < tlv->l; i++) {
-                printf ("%x", *((tlv->v)+i));
+        if (result->addattrs->first) {
+            tlv = (struct tlv *)result->addattrs->first->data;
+            if (!eqtlv(tlv, expected)) {
+                printf ("tlv value was: 0x");
+                for (i = 0; i < tlv->l; i++) {
+                    printf ("%x", *((tlv->v)+i));
+                }
+                printf ("\n");
+                printf ("not ");
             }
-            printf ("\n");
-            printf ("not ");
+            printf("ok %d - rewrite config\n", numtests++);
+        } else {
+            printf("not ok %d - rewrite config\n", numtests++);
         }
-        printf("ok %d - rewrite config\n", numtests++);
-    } else {
-        printf("not ok %d - rewrite ocnfig\n", numtests++);
+
+        freetlv(expected);
     }
 
-    freetlv(expected);
+    /* test issue #62 */
+    {
+        char *expectreplace = "\\1=86400";
+        char **modvattrs = malloc(2);
+        rewritename= "issue62";
+
+        modvattrs[0] = stringcopy("9:102:/^(h323-credit-time).*$/\\1=86400/",0);
+        modvattrs[1] = NULL;
+
+        addrewrite(rewritename, 0, NULL, NULL, NULL, NULL, NULL, modvattrs, NULL, NULL);
+        result = getrewrite(rewritename, NULL);
+
+        if (result && result->modvattrs && result->modvattrs->first) {
+            struct modattr *mod = (struct modattr *)result->modvattrs->first->data;
+            if (regexec(mod->regex,"h323-credit-time=1846422",0,NULL,0)) {
+                printf("not ");
+            }
+            if (strcmp(mod->replacement, expectreplace)) {
+                printf("not ");
+            }
+            else if (mod->t != 102 || mod->vendor != 9) {
+                printf("not ");
+            }
+            printf("ok %d - rewrite config issue #62\n", numtests++);
+        } else {
+            printf("not ok %d - rewrite config issue #62\n", numtests++);
+        }
+
+        free(modvattrs);
+    }
 
     return 0;
 }
