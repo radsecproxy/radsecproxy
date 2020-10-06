066fdd66a0df0e4c93dd2d0cfb0a651e111bfb5d
Fabian Mauchle <mauchle@macfma.switch.ch>
2020-10-06 13:24:39 +0200

    add tests for explicit matchcertificateattribute

--- tests/t_verify_cert.c
+++ tests/t_verify_cert.c
@@ -8,109 +8,7 @@
 #include "../radsecproxy.h"
 #include "../debug.h"
 #include "../hostport.h"
-
-/* /CN=test */
-char *certsimplepem = "-----BEGIN CERTIFICATE-----\n\
-MIHAMIGMAgkAx2VNeC1d5FswCQYHKoZIzj0EATAPMQ0wCwYDVQQDDAR0ZXN0MB4X\n\
-DTIwMDkyODE0MTEzMloXDTIwMTAwODE0MTEzMlowDzENMAsGA1UEAwwEdGVzdDAy\n\
-MBAGByqGSM49AgEGBSuBBAAGAx4ABJxnszX24oQMNcK0IZozUpupFkD/dWBC37qI\n\
-QW4wCQYHKoZIzj0EAQMkADAhAg8Ajl0dHSkadggaqZiD72ACDjWHqYhaIAWTstBv\n\
-g/Q5\n\
------END CERTIFICATE-----";
-
-/* /CN=other */
-char *certsimpleotherpem = "-----BEGIN CERTIFICATE-----\n\
-MIHDMIGOAgkAwf1w/+YshIwwCQYHKoZIzj0EATAQMQ4wDAYDVQQDDAVvdGhlcjAe\n\
-Fw0yMDA5MjkwNTE1MjlaFw0yMDEwMDkwNTE1MjlaMBAxDjAMBgNVBAMMBW90aGVy\n\
-MDIwEAYHKoZIzj0CAQYFK4EEAAYDHgAEnGezNfbihAw1wrQhmjNSm6kWQP91YELf\n\
-uohBbjAJBgcqhkjOPQQBAyUAMCICDwDD9T+qjNHU461al3c11gIPAMZbk5wkhd6C\n\
-ybOsj/PY\n\
------END CERTIFICATE-----";
-
-/* /CN=test, SAN DNS:test.local */
-char *certsandnspem = "-----BEGIN CERTIFICATE-----\n\
-MIHrMIG3oAMCAQICFGNCMLUfhveEcLQmEnX2DqjwFZpGMAkGByqGSM49BAEwDzEN\n\
-MAsGA1UEAwwEdGVzdDAeFw0yMDA5MjkxNjA4NTRaFw0yMDEwMDkxNjA4NTRaMA8x\n\
-DTALBgNVBAMMBHRlc3QwMjAQBgcqhkjOPQIBBgUrgQQABgMeAAScZ7M19uKEDDXC\n\
-tCGaM1KbqRZA/3VgQt+6iEFuoxkwFzAVBgNVHREEDjAMggp0ZXN0LmxvY2FsMAkG\n\
-ByqGSM49BAEDJAAwIQIPAId8FJW00y8XSFmd2lBvAg5K6WAMIFgjhtwcRFcfQg==\n\
------END CERTIFICATE-----";
-
-/* /CN=other, SAN DNS:other.local */
-char *certsandnsotherpem = "-----BEGIN CERTIFICATE-----\n\
-MIHuMIG6oAMCAQICFAiFPNqpXcSIwxS0bfJZs8KDDafVMAkGByqGSM49BAEwEDEO\n\
-MAwGA1UEAwwFb3RoZXIwHhcNMjAwOTI5MTYxMTM2WhcNMjAxMDA5MTYxMTM2WjAQ\n\
-MQ4wDAYDVQQDDAVvdGhlcjAyMBAGByqGSM49AgEGBSuBBAAGAx4ABJxnszX24oQM\n\
-NcK0IZozUpupFkD/dWBC37qIQW6jGjAYMBYGA1UdEQQPMA2CC290aGVyLmxvY2Fs\n\
-MAkGByqGSM49BAEDJAAwIQIOTrQCgOkGcknZEchJFDgCDwCY84F0R2BnNEba95o9\n\
-NA==\n\
------END CERTIFICATE-----";
-
-/* /CN=test, SAN IP Address:192.0.2.1 */
-char *certsanippem = "-----BEGIN CERTIFICATE-----\n\
-MIHlMIGxoAMCAQICFEukd75rE75+qB95Bo7fcb9wXlA9MAkGByqGSM49BAEwDzEN\n\
-MAsGA1UEAwwEdGVzdDAeFw0yMDA5MjkwNTQ5MjZaFw0yMDEwMDkwNTQ5MjZaMA8x\n\
-DTALBgNVBAMMBHRlc3QwMjAQBgcqhkjOPQIBBgUrgQQABgMeAAScZ7M19uKEDDXC\n\
-tCGaM1KbqRZA/3VgQt+6iEFuoxMwETAPBgNVHREECDAGhwTAAAIBMAkGByqGSM49\n\
-BAEDJAAwIQIPALa7jf16ypPNHJSLiotwAg4DnSeToNmbqlRsvM80Aw==\n\
------END CERTIFICATE-----";
-
-/* /CN=other, SAN IP Address:192.0.2.2 */
-char *certsanipotherpem = "-----BEGIN CERTIFICATE-----\n\
-MIHmMIGzoAMCAQICFCYvcOo1Lqc+9JbYqfby1S9rJWufMAkGByqGSM49BAEwEDEO\n\
-MAwGA1UEAwwFb3RoZXIwHhcNMjAwOTI5MTU0OTM2WhcNMjAxMDA5MTU0OTM2WjAQ\n\
-MQ4wDAYDVQQDDAVvdGhlcjAyMBAGByqGSM49AgEGBSuBBAAGAx4ABJxnszX24oQM\n\
-NcK0IZozUpupFkD/dWBC37qIQW6jEzARMA8GA1UdEQQIMAaHBMAAAgIwCQYHKoZI\n\
-zj0EAQMjADAgAg5trehJeRpM04SJJZ6XnAIOFfzRRnQtm5rnsP+QBe8=\n\
------END CERTIFICATE-----";
-
-/* /CN=test, SAN IP Address:2001:DB8:0:0:0:0:0:1 */
-char *certsanipv6pem = "-----BEGIN CERTIFICATE-----\n\
-MIHxMIG9oAMCAQICFGhkABYXfolor1EF6Li3hDQeEVU+MAkGByqGSM49BAEwDzEN\n\
-MAsGA1UEAwwEdGVzdDAeFw0yMDA5MjkxNTU1NTZaFw0yMDEwMDkxNTU1NTZaMA8x\n\
-DTALBgNVBAMMBHRlc3QwMjAQBgcqhkjOPQIBBgUrgQQABgMeAAScZ7M19uKEDDXC\n\
-tCGaM1KbqRZA/3VgQt+6iEFuox8wHTAbBgNVHREEFDAShxAgAQ24AAAAAAAAAAAA\n\
-AAABMAkGByqGSM49BAEDJAAwIQIPAKsn++FWaDIcpnNBOFTuAg5C7gs7DxaNWgEu\n\
-OrBTXA==\n\
------END CERTIFICATE-----";
-
-/* /CN=test, SAN DNS:192.0.2.1 */
-char *certsanipindnspem = "-----BEGIN CERTIFICATE-----\n\
-MIHqMIG2oAMCAQICFFUjZGG96kpFI2fu90+jAhWsTr8YMAkGByqGSM49BAEwDzEN\n\
-MAsGA1UEAwwEdGVzdDAeFw0yMDA5MjkxNTU4NDBaFw0yMDEwMDkxNTU4NDBaMA8x\n\
-DTALBgNVBAMMBHRlc3QwMjAQBgcqhkjOPQIBBgUrgQQABgMeAAScZ7M19uKEDDXC\n\
-tCGaM1KbqRZA/3VgQt+6iEFuoxgwFjAUBgNVHREEDTALggkxOTIuMC4yLjEwCQYH\n\
-KoZIzj0EAQMkADAhAg5BngyplTbRlQ8o/oWWwQIPAL9SfgIaXi/gD6YlQCOU\n\
------END CERTIFICATE-----";
-
-/* /CN=test, SAN DNS:2001:DB8::1 */
-char *certsanipv6indnspem = "-----BEGIN CERTIFICATE-----\n\
-MIHsMIG4oAMCAQICFFgnXltbOEGcWsS0vCv6Lsj4FhO3MAkGByqGSM49BAEwDzEN\n\
-MAsGA1UEAwwEdGVzdDAeFw0yMDA5MjkxNjAyMDRaFw0yMDEwMDkxNjAyMDRaMA8x\n\
-DTALBgNVBAMMBHRlc3QwMjAQBgcqhkjOPQIBBgUrgQQABgMeAAScZ7M19uKEDDXC\n\
-tCGaM1KbqRZA/3VgQt+6iEFuoxowGDAWBgNVHREEDzANggsyMDAxOmRiODo6MTAJ\n\
-BgcqhkjOPQQBAyQAMCECDlWFhJxpHRgt93ZzN9k7Ag8Ag0YN+dL3MEIo2sqgRWg=\n\
------END CERTIFICATE-----";
-
-/* /CN=test, DNS:somethinglese, DNS:test.local, IP Address:192.0.2.1, IP Address:2001:DB8:0:0:0:0:0:1 */
-char *certcomplexpem = "-----BEGIN CERTIFICATE-----\n\
-MIIBEjCB3qADAgECAhRgxyW7klgZvTf9isALCvwlVwvRtDAJBgcqhkjOPQQBMA8x\n\
-DTALBgNVBAMMBHRlc3QwHhcNMjAwOTMwMDU1MjIyWhcNMjAxMDEwMDU1MjIyWjAP\n\
-MQ0wCwYDVQQDDAR0ZXN0MDIwEAYHKoZIzj0CAQYFK4EEAAYDHgAEnGezNfbihAw1\n\
-wrQhmjNSm6kWQP91YELfuohBbqNAMD4wPAYDVR0RBDUwM4INc29tZXRoaW5nbGVz\n\
-ZYIKdGVzdC5sb2NhbIcEwAACAYcQIAENuAAAAAAAAAAAAAAAATAJBgcqhkjOPQQB\n\
-AyQAMCECDlTfJfMJElZZgvUdkatdAg8ApiXkPXLXXrV6OMRG9us=\n\
------END CERTIFICATE-----";
-
-/* /CN=test, DNS:somethinglese, DNS:other.local, IP Address:192.0.2.2, IP Address:2001:DB8:0:0:0:0:0:2 */
-char *certcomplexotherpem = "-----BEGIN CERTIFICATE-----\n\
-MIIBFTCB4aADAgECAhR0GSgeV7pqQnbHRgv5y5plz/6+NjAJBgcqhkjOPQQBMBAx\n\
-DjAMBgNVBAMMBW90aGVyMB4XDTIwMDkzMDA1NTI1NVoXDTIwMTAxMDA1NTI1NVow\n\
-EDEOMAwGA1UEAwwFb3RoZXIwMjAQBgcqhkjOPQIBBgUrgQQABgMeAAScZ7M19uKE\n\
-DDXCtCGaM1KbqRZA/3VgQt+6iEFuo0EwPzA9BgNVHREENjA0gg1zb21ldGhpbmds\n\
-ZXNlggtvdGhlci5sb2NhbIcEwAACAocQIAENuAAAAAAAAAAAAAAAAjAJBgcqhkjO\n\
-PQQBAyQAMCECDwCEaHL6oHT4zwH6jD91YwIOYO3L8cHIzmnGCOJYIQ4=\n\
------END CERTIFICATE-----";
+#include "../util.h"
 
 X509 *getcert(char *pem) {
     X509* certX509;
@@ -134,24 +32,152 @@ void ok(int expect, int result, char *descr) {
     }
 }
 
+void free_match_srvconf(struct clsrvconf *conf) {
+    free(conf->matchcertattr);
+    conf->matchcertattr = NULL;
+    if (conf->certcnregex)
+        regfree(conf->certcnregex);
+    free(conf->certcnregex);
+    conf->certcnregex = NULL;
+    if (conf->certuriregex)
+        regfree(conf->certuriregex);
+    free(conf->certuriregex);
+    conf->certuriregex = NULL;
+    if (conf->certdnsregex)
+        regfree(conf->certdnsregex);
+    free(conf->certdnsregex);
+    conf->certdnsregex = NULL;
+    memset(&conf->certipmatch, 0, sizeof(struct in6_addr));
+    conf->certipmatchaf=0;
+}
+
 int
 main (int argc, char *argv[])
 {
     struct clsrvconf conf;
-    X509 *certsimple, *certsimpleother, *certsandns, *certsandnsother, *certsanip, *certsanipother, *certsanipindns,
-        *certsanipv6, *certsanipv6indns, *certcomplex, *certcomplexother;
+    X509 
+    /* /CN=test */
+    *certsimple = getcert("-----BEGIN CERTIFICATE-----\n\
+MIHAMIGMAgkAx2VNeC1d5FswCQYHKoZIzj0EATAPMQ0wCwYDVQQDDAR0ZXN0MB4X\n\
+DTIwMDkyODE0MTEzMloXDTIwMTAwODE0MTEzMlowDzENMAsGA1UEAwwEdGVzdDAy\n\
+MBAGByqGSM49AgEGBSuBBAAGAx4ABJxnszX24oQMNcK0IZozUpupFkD/dWBC37qI\n\
+QW4wCQYHKoZIzj0EAQMkADAhAg8Ajl0dHSkadggaqZiD72ACDjWHqYhaIAWTstBv\n\
+g/Q5\n\
+-----END CERTIFICATE-----"),
 
-    certsimple = getcert(certsimplepem);
-    certsimpleother = getcert(certsimpleotherpem);
-    certsandns = getcert(certsandnspem);
-    certsandnsother = getcert(certsandnsotherpem);
-    certsanip = getcert(certsanippem);
-    certsanipother = getcert(certsanipotherpem);
-    certsanipindns = getcert(certsanipindnspem);
-    certsanipv6 = getcert(certsanipv6pem);
-    certsanipv6indns = getcert(certsanipv6indnspem);
-    certcomplex = getcert(certcomplexpem);
-    certcomplexother = getcert(certcomplexotherpem);
+    /* /CN=other */
+    *certsimpleother = getcert("-----BEGIN CERTIFICATE-----\n\
+MIHDMIGOAgkAwf1w/+YshIwwCQYHKoZIzj0EATAQMQ4wDAYDVQQDDAVvdGhlcjAe\n\
+Fw0yMDA5MjkwNTE1MjlaFw0yMDEwMDkwNTE1MjlaMBAxDjAMBgNVBAMMBW90aGVy\n\
+MDIwEAYHKoZIzj0CAQYFK4EEAAYDHgAEnGezNfbihAw1wrQhmjNSm6kWQP91YELf\n\
+uohBbjAJBgcqhkjOPQQBAyUAMCICDwDD9T+qjNHU461al3c11gIPAMZbk5wkhd6C\n\
+ybOsj/PY\n\
+-----END CERTIFICATE-----"),
+
+/* /CN=test, SAN DNS:test.local */
+*certsandns =  getcert("-----BEGIN CERTIFICATE-----\n\
+MIHrMIG3oAMCAQICFGNCMLUfhveEcLQmEnX2DqjwFZpGMAkGByqGSM49BAEwDzEN\n\
+MAsGA1UEAwwEdGVzdDAeFw0yMDA5MjkxNjA4NTRaFw0yMDEwMDkxNjA4NTRaMA8x\n\
+DTALBgNVBAMMBHRlc3QwMjAQBgcqhkjOPQIBBgUrgQQABgMeAAScZ7M19uKEDDXC\n\
+tCGaM1KbqRZA/3VgQt+6iEFuoxkwFzAVBgNVHREEDjAMggp0ZXN0LmxvY2FsMAkG\n\
+ByqGSM49BAEDJAAwIQIPAId8FJW00y8XSFmd2lBvAg5K6WAMIFgjhtwcRFcfQg==\n\
+-----END CERTIFICATE-----"),
+
+/* /CN=other, SAN DNS:other.local */
+*certsandnsother =  getcert("-----BEGIN CERTIFICATE-----\n\
+MIHuMIG6oAMCAQICFAiFPNqpXcSIwxS0bfJZs8KDDafVMAkGByqGSM49BAEwEDEO\n\
+MAwGA1UEAwwFb3RoZXIwHhcNMjAwOTI5MTYxMTM2WhcNMjAxMDA5MTYxMTM2WjAQ\n\
+MQ4wDAYDVQQDDAVvdGhlcjAyMBAGByqGSM49AgEGBSuBBAAGAx4ABJxnszX24oQM\n\
+NcK0IZozUpupFkD/dWBC37qIQW6jGjAYMBYGA1UdEQQPMA2CC290aGVyLmxvY2Fs\n\
+MAkGByqGSM49BAEDJAAwIQIOTrQCgOkGcknZEchJFDgCDwCY84F0R2BnNEba95o9\n\
+NA==\n\
+-----END CERTIFICATE-----"),
+
+/* /CN=test, SAN IP Address:192.0.2.1 */
+*certsanip =  getcert("-----BEGIN CERTIFICATE-----\n\
+MIHlMIGxoAMCAQICFEukd75rE75+qB95Bo7fcb9wXlA9MAkGByqGSM49BAEwDzEN\n\
+MAsGA1UEAwwEdGVzdDAeFw0yMDA5MjkwNTQ5MjZaFw0yMDEwMDkwNTQ5MjZaMA8x\n\
+DTALBgNVBAMMBHRlc3QwMjAQBgcqhkjOPQIBBgUrgQQABgMeAAScZ7M19uKEDDXC\n\
+tCGaM1KbqRZA/3VgQt+6iEFuoxMwETAPBgNVHREECDAGhwTAAAIBMAkGByqGSM49\n\
+BAEDJAAwIQIPALa7jf16ypPNHJSLiotwAg4DnSeToNmbqlRsvM80Aw==\n\
+-----END CERTIFICATE-----"),
+
+/* /CN=other, SAN IP Address:192.0.2.2 */
+*certsanipother =  getcert("-----BEGIN CERTIFICATE-----\n\
+MIHmMIGzoAMCAQICFCYvcOo1Lqc+9JbYqfby1S9rJWufMAkGByqGSM49BAEwEDEO\n\
+MAwGA1UEAwwFb3RoZXIwHhcNMjAwOTI5MTU0OTM2WhcNMjAxMDA5MTU0OTM2WjAQ\n\
+MQ4wDAYDVQQDDAVvdGhlcjAyMBAGByqGSM49AgEGBSuBBAAGAx4ABJxnszX24oQM\n\
+NcK0IZozUpupFkD/dWBC37qIQW6jEzARMA8GA1UdEQQIMAaHBMAAAgIwCQYHKoZI\n\
+zj0EAQMjADAgAg5trehJeRpM04SJJZ6XnAIOFfzRRnQtm5rnsP+QBe8=\n\
+-----END CERTIFICATE-----"),
+
+/* /CN=test, SAN IP Address:2001:DB8:0:0:0:0:0:1 */
+*certsanipv6 =  getcert("-----BEGIN CERTIFICATE-----\n\
+MIHxMIG9oAMCAQICFGhkABYXfolor1EF6Li3hDQeEVU+MAkGByqGSM49BAEwDzEN\n\
+MAsGA1UEAwwEdGVzdDAeFw0yMDA5MjkxNTU1NTZaFw0yMDEwMDkxNTU1NTZaMA8x\n\
+DTALBgNVBAMMBHRlc3QwMjAQBgcqhkjOPQIBBgUrgQQABgMeAAScZ7M19uKEDDXC\n\
+tCGaM1KbqRZA/3VgQt+6iEFuox8wHTAbBgNVHREEFDAShxAgAQ24AAAAAAAAAAAA\n\
+AAABMAkGByqGSM49BAEDJAAwIQIPAKsn++FWaDIcpnNBOFTuAg5C7gs7DxaNWgEu\n\
+OrBTXA==\n\
+-----END CERTIFICATE-----"),
+
+/* /CN=test, SAN DNS:192.0.2.1 */
+*certsanipindns =  getcert("-----BEGIN CERTIFICATE-----\n\
+MIHqMIG2oAMCAQICFFUjZGG96kpFI2fu90+jAhWsTr8YMAkGByqGSM49BAEwDzEN\n\
+MAsGA1UEAwwEdGVzdDAeFw0yMDA5MjkxNTU4NDBaFw0yMDEwMDkxNTU4NDBaMA8x\n\
+DTALBgNVBAMMBHRlc3QwMjAQBgcqhkjOPQIBBgUrgQQABgMeAAScZ7M19uKEDDXC\n\
+tCGaM1KbqRZA/3VgQt+6iEFuoxgwFjAUBgNVHREEDTALggkxOTIuMC4yLjEwCQYH\n\
+KoZIzj0EAQMkADAhAg5BngyplTbRlQ8o/oWWwQIPAL9SfgIaXi/gD6YlQCOU\n\
+-----END CERTIFICATE-----"),
+
+/* /CN=test, SAN DNS:2001:DB8::1 */
+*certsanipv6indns =  getcert("-----BEGIN CERTIFICATE-----\n\
+MIHsMIG4oAMCAQICFFgnXltbOEGcWsS0vCv6Lsj4FhO3MAkGByqGSM49BAEwDzEN\n\
+MAsGA1UEAwwEdGVzdDAeFw0yMDA5MjkxNjAyMDRaFw0yMDEwMDkxNjAyMDRaMA8x\n\
+DTALBgNVBAMMBHRlc3QwMjAQBgcqhkjOPQIBBgUrgQQABgMeAAScZ7M19uKEDDXC\n\
+tCGaM1KbqRZA/3VgQt+6iEFuoxowGDAWBgNVHREEDzANggsyMDAxOmRiODo6MTAJ\n\
+BgcqhkjOPQQBAyQAMCECDlWFhJxpHRgt93ZzN9k7Ag8Ag0YN+dL3MEIo2sqgRWg=\n\
+-----END CERTIFICATE-----"),
+
+/* /CN=test, DNS:somethinglese, DNS:test.local, IP Address:192.0.2.1, IP Address:2001:DB8:0:0:0:0:0:1 */
+*certcomplex =  getcert("-----BEGIN CERTIFICATE-----\n\
+MIIBEjCB3qADAgECAhRgxyW7klgZvTf9isALCvwlVwvRtDAJBgcqhkjOPQQBMA8x\n\
+DTALBgNVBAMMBHRlc3QwHhcNMjAwOTMwMDU1MjIyWhcNMjAxMDEwMDU1MjIyWjAP\n\
+MQ0wCwYDVQQDDAR0ZXN0MDIwEAYHKoZIzj0CAQYFK4EEAAYDHgAEnGezNfbihAw1\n\
+wrQhmjNSm6kWQP91YELfuohBbqNAMD4wPAYDVR0RBDUwM4INc29tZXRoaW5nbGVz\n\
+ZYIKdGVzdC5sb2NhbIcEwAACAYcQIAENuAAAAAAAAAAAAAAAATAJBgcqhkjOPQQB\n\
+AyQAMCECDlTfJfMJElZZgvUdkatdAg8ApiXkPXLXXrV6OMRG9us=\n\
+-----END CERTIFICATE-----"),
+
+/* /CN=test, DNS:somethinglese, DNS:other.local, IP Address:192.0.2.2, IP Address:2001:DB8:0:0:0:0:0:2 */
+*certcomplexother =  getcert("-----BEGIN CERTIFICATE-----\n\
+MIIBFTCB4aADAgECAhR0GSgeV7pqQnbHRgv5y5plz/6+NjAJBgcqhkjOPQQBMBAx\n\
+DjAMBgNVBAMMBW90aGVyMB4XDTIwMDkzMDA1NTI1NVoXDTIwMTAxMDA1NTI1NVow\n\
+EDEOMAwGA1UEAwwFb3RoZXIwMjAQBgcqhkjOPQIBBgUrgQQABgMeAAScZ7M19uKE\n\
+DDXCtCGaM1KbqRZA/3VgQt+6iEFuo0EwPzA9BgNVHREENjA0gg1zb21ldGhpbmds\n\
+ZXNlggtvdGhlci5sb2NhbIcEwAACAocQIAENuAAAAAAAAAAAAAAAAjAJBgcqhkjO\n\
+PQQBAyQAMCECDwCEaHL6oHT4zwH6jD91YwIOYO3L8cHIzmnGCOJYIQ4=\n\
+-----END CERTIFICATE-----"),
+        
+    /* /CN=test, URI:https://test.local/profile#me */
+    *certsanuri = getcert("-----BEGIN CERTIFICATE-----\n\
+MIH9MIHKoAMCAQICFHsSOjcYexRKQpNlH1oHV1cxvdgHMAkGByqGSM49BAEwDzEN\n\
+MAsGA1UEAwwEdGVzdDAeFw0yMDEwMDYwODU5MzRaFw0yMDEwMTYwODU5MzRaMA8x\n\
+DTALBgNVBAMMBHRlc3QwMjAQBgcqhkjOPQIBBgUrgQQABgMeAAScZ7M19uKEDDXC\n\
+tCGaM1KbqRZA/3VgQt+6iEFuoywwKjAoBgNVHREEITAfhh1odHRwczovL3Rlc3Qu\n\
+bG9jYWwvcHJvZmlsZSNtZTAJBgcqhkjOPQQBAyMAMCACDniwUmV285CoguiJ6WmW\n\
+Ag5ZWNTJtmNNdKxh0Mahsw==\n\
+-----END CERTIFICATE-----"),
+
+        /* /CN=other, URI:https://other.local/profile#me */
+        *certsanuriother = getcert("-----BEGIN CERTIFICATE-----\n\
+MIIBATCBzaADAgECAhQLG7rYpl+8YbPNEtUgw6HRZYIc1DAJBgcqhkjOPQQBMBAx\n\
+DjAMBgNVBAMMBW90aGVyMB4XDTIwMTAwNjA5MDU0OVoXDTIwMTAxNjA5MDU0OVow\n\
+EDEOMAwGA1UEAwwFb3RoZXIwMjAQBgcqhkjOPQIBBgUrgQQABgMeAAScZ7M19uKE\n\
+DDXCtCGaM1KbqRZA/3VgQt+6iEFuoy0wKzApBgNVHREEIjAghh5odHRwczovL290\n\
+aGVyLmxvY2FsL3Byb2ZpbGUjbWUwCQYHKoZIzj0EAQMkADAhAg8AoOJVnRcp3gyY\n\
+Qe0Vy/UCDijCHK6Y5GkzWD7H008l\n\
+-----END CERTIFICATE-----");
 
     memset(&conf, 0, sizeof(conf));
     conf.hostports = list_create();
@@ -236,6 +262,7 @@ main (int argc, char *argv[])
     /* test literal ipv6 match to SAN IP */
     {
         struct hostportres hp;
+        memset(&hp, 0, sizeof(struct hostportres));
 
         conf.name = "test";
         conf.certnamecheck = 1;
@@ -314,10 +341,145 @@ main (int argc, char *argv[])
         while(list_shift(conf.hostports));
     }
 
-    //TODO explicit CN/SAN checks
-    //TODO explicit & implicit combined
+    /* test explicit CN regex */
+    {
+        conf.name = "test";
+        conf.certnamecheck = 0;
+        conf.matchcertattr = stringcopy("CN:/t..t/",0);
+
+        ok(1,addmatchcertattr(&conf),"explicit cn regex config");
+
+        ok(1,verifyconfcert(certsimple, &conf),"explicit cn regex");
+        ok(0,verifyconfcert(certsimpleother, &conf),"negative explicit cn regex");
+        ok(1,verifyconfcert(certsandns, &conf), "explicit cn regex with SAN DNS");
+
+        free_match_srvconf(&conf);
+    }
+
+    /* test explicit ip match to SAN IP */
+    {
+        conf.name = "test";
+        conf.certnamecheck = 0;
+        conf.matchcertattr = stringcopy("SubjectAltName:IP:192.0.2.1",0);
+
+        ok(1,addmatchcertattr(&conf),"explicit san ip config");
+
+
+        ok(1,verifyconfcert(certsanip, &conf),"explicit san ip");
+        ok(0,verifyconfcert(certsanipother, &conf),"wrong explicit san ip");
+        ok(0,verifyconfcert(certsimple, &conf), "missing explicit san ip");
+        ok(1,verifyconfcert(certcomplex,&conf),"explicit san ip in complex cert");
+
+        free_match_srvconf(&conf);
+    }
+
+    /* test explicit ipv6 match to SAN IP */
+    {
+
+        conf.name = "test";
+        conf.certnamecheck = 0;
+        conf.matchcertattr = stringcopy("SubjectAltName:IP:2001:db8::1",0);
+
+        ok(1,addmatchcertattr(&conf),"explicit san ipv6 config");
+
+
+        ok(1,verifyconfcert(certsanipv6, &conf),"explicit san ipv6");
+        ok(0,verifyconfcert(certsanipother, &conf),"wrong explicit san ipv6");
+        ok(0,verifyconfcert(certsimple, &conf),"missing explicitsan ipv6");
+        ok(1,verifyconfcert(certcomplex,&conf),"explicit san ipv6 in complex cert");
+
+        free_match_srvconf(&conf);
+    }
+
+    /* test explicit SAN DNS regex */
+    {
+        conf.name = "test";
+        conf.certnamecheck = 0;
+        conf.matchcertattr = stringcopy("SubjectAltName:DNS:/t..t\\.local/",0);
+
+        ok(1,addmatchcertattr(&conf),"explicit san dns regex config");
+
+        ok(1,verifyconfcert(certsandns, &conf),"explicit san dns");
+        ok(0,verifyconfcert(certsandnsother, &conf),"negative explicit san dns");
+        ok(0,verifyconfcert(certsimple,&conf),"missing explicit san dns");
+        ok(1,verifyconfcert(certcomplex,&conf),"explicit san dns in complex cert");
+
+        free_match_srvconf(&conf);
+    }
+    /* test explicit SAN URI regex */
+    {
+        conf.name = "test";
+        conf.certnamecheck = 0;
+        conf.matchcertattr = stringcopy("SubjectAltName:URI:/https:\\/\\/test.local\\/profile#me/",0);
+
+        ok(1,addmatchcertattr(&conf),"explicit cn regex config");
+
+        ok(1,verifyconfcert(certsanuri, &conf),"explicit san uri regex");
+        ok(0,verifyconfcert(certsanuriother, &conf),"negative explicit san uri");
+        ok(0,verifyconfcert(certsimple, &conf), "missing explicit san uri");
+
+        free_match_srvconf(&conf);
+    }
+
+    /* test valid config syntax */
+    {
+        conf.name = "test";
+        conf.certnamecheck = 0;
+        conf.matchcertattr = stringcopy("CN:/t..t",0);
+
+        ok(1,addmatchcertattr(&conf),"test regex config syntax");
+        ok(1,verifyconfcert(certsimple, &conf),"test regex config syntax execution");
+
+        free_match_srvconf(&conf);
+    }
+
+    /* test invalid config syntax */
+    {
+        conf.name = "test";
+        conf.certnamecheck = 0;
+
+        conf.matchcertattr = stringcopy("CN:t..t",0);
+        ok(0,addmatchcertattr(&conf),"test invalid syntax regex");
+        free_match_srvconf(&conf);
+
+        conf.matchcertattr = stringcopy("SAN:/t..t/",0);
+        ok(0,addmatchcertattr(&conf),"test invalid syntax attribute");
+        free_match_srvconf(&conf);
+
+        conf.matchcertattr = stringcopy("SubjectAltName:IP:1.2.3",0);
+        ok(0,addmatchcertattr(&conf),"test invalid syntax ip");
+        free_match_srvconf(&conf);
+
+        conf.matchcertattr = stringcopy("SubjectAltName:IP:2001:db8:1",0);
+        ok(0,addmatchcertattr(&conf),"test invalid syntax ipv6");
+        free_match_srvconf(&conf);
+    }
+
+    /* test explicit & implicit combined */
+    {
+        struct hostportres hp;
+
+        conf.name = "test";
+        conf.certnamecheck = 1;
+        conf.matchcertattr = stringcopy("CN:/t..t",0);
+        hp.host = "test.local";
+        hp.prefixlen = 255;
+        list_push(conf.hostports, &hp);
+
+        ok(1,addmatchcertattr(&conf),"combined config");
+
+        ok(1,verifyconfcert(certsandns, &conf),"combined san dns");
+        ok(0,verifyconfcert(certsandnsother, &conf),"negative combined san dns");
+        ok(1,verifyconfcert(certcomplex,&conf),"combined san dns in complex cert");
+        ok(0,verifyconfcert(certsimple, &conf),"combined missing san dns");
+
+        while(list_shift(conf.hostports));
+        free_match_srvconf(&conf);
+    }
+
 
     printf("1..%d\n", numtests);
+    list_free(conf.hostports);
     X509_free(certsimple);
     X509_free(certsimpleother);
     X509_free(certsandns);
@@ -329,6 +491,8 @@ main (int argc, char *argv[])
     X509_free(certsanipv6indns);
     X509_free(certcomplex);
     X509_free(certcomplexother);
+    X509_free(certsanuri);
+    X509_free(certsanuriother);
 
     return 0;
 }
