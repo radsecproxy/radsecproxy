6a4f74fdb323bb1310ae871721a572a994906fab
Jan-Frederik Rieckers <rieckers@dfn.de>
2023-04-12 22:08:06 +0200

    Add configuration option IgnoreSRVHost
    This is intended to ignore certain hosts for dynamic discovery.

--- radsecproxy.h
+++ radsecproxy.h
@@ -157,6 +157,7 @@ struct clsrvconf {
     char *confrewriteusername;
     struct modattr *rewriteusername;
     char *dynamiclookupcommand;
+    char **ignore_srv_host;
     enum rsp_statsrv statusserver;
     uint8_t retryinterval;
     uint8_t retrycount;
